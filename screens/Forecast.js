63cecd259d69e0e02545142da61de5b24f538f88
misharexviashvili <misharexviashvili@yahoo.com>
2023-04-05 01:25:01 +0400

    back button added in forecast screen

--- screens/Forecast.js
+++ screens/Forecast.js
@@ -1,8 +1,8 @@
-import { ScrollView } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import ForecastOutput from "../components/weather elements/ForecastOutput";
 import { LinearGradient } from "expo-linear-gradient";
-export default function Forecast({ route }) {
+import { Ionicons } from "@expo/vector-icons";
+export default function Forecast({ navigation, route }) {
   // console.log("params", route.params);
   const forecast = route.params.forecast.forecastday;
   let isDay = route.params.current.is_day;
@@ -16,11 +16,21 @@ export default function Forecast({ route }) {
       }
     >
       <ScrollView style={styles.scrollView}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.pressable,
+            pressed ? styles.pressed : null,
+          ]}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="arrow-back-outline" size={30} />
+          <Text style={styles.btnText}>Return</Text>
+        </Pressable>
         <ForecastOutput forecast={forecast[0]} />
         <ForecastOutput forecast={forecast[1]} />
         <ForecastOutput forecast={forecast[2]} />
-        {/* Free plan did not include more than 3 days */}
-        {/* <ForecastOutput forecast={forecast[3]} />
+        {/* Free plan did not include more than 3 days
+        <ForecastOutput forecast={forecast[3]} />
         <ForecastOutput forecast={forecast[4]} />
         <ForecastOutput forecast={forecast[5]} />
         <ForecastOutput forecast={forecast[6]} /> */}
@@ -35,5 +45,21 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     paddingHorizontal: 10,
+  },
+  pressable: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "25%",
+    paddingHorizontal: 6,
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 20,
+  },
+  btnText: {
+    fontSize: 16,
+  },
+  pressed: {
+    opacity: 0.7,
   },
 });
